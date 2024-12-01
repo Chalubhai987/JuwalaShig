22dbf537a9ceae7835e963d8e9b9a6bd29d10392
Chalubhai987 <visa93379@gmail.com>
2024-12-01 22:28:26 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @DEVILVIPDDOS
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7409506208:AAF3qO0t8tuHQkCiEe4YUZYEdurwOGW_LII")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@SURAJYTOPHACK")
+
+  #Enter your admin id here Get it from @DEVILVIPDDOS by typing /info
+ADMIN_IDS=("6899971263")
+
+
+
+
+  
